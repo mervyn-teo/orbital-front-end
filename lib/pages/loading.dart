f8dbd451e0a12b76b35cf4ce34dcb12c1c011190
nicholasdai <nicholasdaijh@gmail.com>
2024-06-06 23:07:09 +0800

    - made a loading screen yipeeee
    Signed-off-by: nicholasdai <nicholasdaijh@gmail.com>

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class loading extends StatefulWidget {
   const loading({super.key});
@@ -11,13 +12,17 @@ class loading extends StatefulWidget {
 class _loadingState extends State<loading> {
   @override
   Widget build(BuildContext context) {
+    precacheImage(AssetImage('assets/loadingwallpaper.png'), context);
+    precacheImage(AssetImage('assets/logo.png'), context);
+    ImageProvider loadedWallpaper = AssetImage('assets/loadingwallpaper.png');
+    ImageProvider logo = AssetImage('assets/logo.png');
     return Scaffold(
       body: FutureBuilder(
         future: hasLoggedIn(), 
         builder: (BuildContext context, AsyncSnapshot snap) {
           WidgetsBinding.instance.addPostFrameCallback((_){
             if (snap.hasData) {
-            bool hasLoggedIn = snap.data;
+              bool hasLoggedIn = snap.data;
             if (hasLoggedIn) {
               Navigator.popAndPushNamed(context, '/home');
             } else {
@@ -25,10 +30,36 @@ class _loadingState extends State<loading> {
             }
           }
         });
-          return const Text('loading');
-        }),
+        return Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: loadedWallpaper,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image(
+                  image: logo,
+                  width: 300,  // Adjust the size as needed
+                  height: 300, // Adjust the size as needed
+                ),
+                SizedBox(height: 5),// Add spacing between logo and spinner
+                SpinKitPouringHourGlass(
+                  color: Colors.blueAccent,
+                  size: 120.0,
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
+
 
   Future<bool> hasLoggedIn() async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -21,111 +21,124 @@ class _loginState extends State<login> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        backgroundColor: Colors.yellow[50],
         body: Column(children: [
-              Container(
-                padding: const EdgeInsets.fromLTRB(20, 50, 10, 50),
-                alignment: Alignment.center,
-                child: const Text(
-                  "Account Login",
-                  style: TextStyle(
+          SizedBox(height: 150),
+          Container(
+              padding: const EdgeInsets.fromLTRB(20, 50, 10, 0),
+              alignment: Alignment.center,
+              child: const Text(
+                "Account Login",
+                style: TextStyle(
                     fontSize: 40),)
-                ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                margin: const EdgeInsets.fromLTRB(20, 10, 20, 0) ,
-                child: TextField(
-                  style: const TextStyle(fontSize: 20),
-                  enableSuggestions: true,
-                  maxLines: 1,
-                  onChanged: (value) {email = value;},
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "e-mail", ),
-                  keyboardType: TextInputType.emailAddress,)),
-              Container(
-                padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                margin: const EdgeInsets.fromLTRB(20, 10, 20, 0),
-                child: TextField(
+          ),
+          Container(
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              margin: const EdgeInsets.fromLTRB(20, 10, 20, 0) ,
+              child: TextField(
+                style: const TextStyle(fontSize: 20),
+                enableSuggestions: true,
+                maxLines: 1,
+                onChanged: (value) {email = value;},
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "E-mail", ),
+                keyboardType: TextInputType.emailAddress,)),
+          Container(
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              margin: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: TextField(
                   style: const TextStyle(fontSize: 20),
                   maxLines: 1,
                   onChanged: (value) {password = value;},
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    labelText: "password", ),
-                  obscureText: true, 
-                  obscuringCharacter: "*")),
-              Container(
-                margin: const EdgeInsets.fromLTRB(25, 0, 30, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.fromLTRB(10, 0, 0, 0),
-                      child: RichText(
-                          text: TextSpan(
-                            text: "don't have an account? sign up here",
-                            style: const TextStyle(color: Colors.grey),
-                            recognizer: TapGestureRecognizer() ..onTap = () {                        
-                              Navigator.push(context, MaterialPageRoute(builder: (context) => const register()));
-                              }
-                            ),
-                          textAlign: TextAlign.left,
-                        ),
+                    labelText: "Password", ),
+                  obscureText: true,
+                  obscuringCharacter: "*")
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(25, 0, 30, 10),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Don't have an account? ",
+                    style: TextStyle(
+                      color: Colors.grey,
                     ),
-                    MaterialButton(
-                        onPressed: (){
-                          login();
-                        },    // TODO: implement info handling after API response
-                        color: Colors.amber,
-                        child: const Text(
-                          "Login", 
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.black,),
-                      )
-                      ),
-                    ]),
-              ),
+                  ),
+                  TextSpan(
+                    text: "Sign up here",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      decoration: TextDecoration.underline,
+                    ),
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => register()),
+                        );
+                      },
+                  ),
                 ],
               ),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          MaterialButton(
+              onPressed: () {
+                login();
+              },    // TODO: implement info handling after API response
+              color: Colors.amber,
+              child: const Text(
+                "Login",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black),
+              )
+          ),
+        ],
         ),
-      );
+      ),
+    );
   }
 
   void login() async {
-     if (!EmailValidator.validate(email)) {
+    if (!EmailValidator.validate(email)) {
       showDialog(
-        context: context, 
-        builder: (context) {
-          return const AlertDialog(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(20)
-              )
+          context: context,
+          builder: (context) {
+            return const AlertDialog(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                      Radius.circular(20)
+                  )
               ),
               title: Text("invalid e-mail!"),
               content: Text("please make sure your email is correct!"),
-          );
-      }
+            );
+          }
       );
     } else {
       String errMsg = await loginResponse(email, password).catchError((e) {
-        return "Server response timeout!";
+        return e.toString();
       });
       if (errMsg != "ok") {
         showDialog(
-          context: context, 
-          builder: (context) {
-            return  AlertDialog(
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20)
-                )
+            context: context,
+            builder: (context) {
+              return  AlertDialog(
+                shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(20)
+                    )
                 ),
                 title: const Text("login failed"),
                 content: Text(errMsg),
-            );
-          }
+              );
+            }
         );
       } else {
         Navigator.popAndPushNamed(context, '/home');
@@ -138,23 +151,26 @@ class _loginState extends State<login> {
     String retStatus = "";
 
     try {
-    final response = await http.post(Uri.parse('http://13.231.75.235:8080/login'),
-      body: jsonEncode(<String, String>{
-        "email": email,
-        "pwd": pwd
-      }))
-      .timeout(const Duration(seconds: 5));
-      
+      final response = await http.post(Uri.parse('http://13.231.75.235:8080/login'),
+          body: jsonEncode(<String, String>{
+            "email": email,
+            "pwd": pwd
+          }))
+          .timeout(const Duration(seconds: 5));
+
       var converted = decoder.convert(response.body);
 
-      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      // check if resonse is not ok
+      if (converted['err_msg'] == "ok") {
+        final SharedPreferences prefs = await SharedPreferences.getInstance();
         await prefs.setString('name', converted['body'][0]['name']);
         await prefs.setString('bio', converted['body'][0]['bio']);
         await prefs.setInt('age', converted['body'][0]['age']);
         await prefs.setString('id', converted['body'][0]['id']);
         await prefs.setString('pfp', converted['body'][0]['pfp']);
+      }
 
-      return converted['err_msg'];  
+      return converted['err_msg'];
     } on Exception catch(e) {
       return e.toString();
     }
